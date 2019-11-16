8ba8c646b07da6ed085b6a57236c8e80dc5e9ac2
NIIBE Yutaka <gniibe@fsij.org>
2019-11-16 12:15:13 +0900

    riscv: Fix chx_idle.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- chopstx-riscv32.c
+++ chopstx-riscv32.c
@@ -398,20 +398,17 @@ chx_init_arch (struct chx_thread *tp)
 
 
 
-static void __attribute__((naked))
+static void __attribute__((used,naked))
 chx_idle (void)
 {
-  /*TBD*/
-  int sleep_enabled;
+  extern void chx_prepare_sleep_mode (void);
 
   for (;;)
     {
-      asm ("lw	%0,%1" : "=r" (sleep_enabled): "m" (chx_allow_sleep));
-      if (sleep_enabled)
-	{
-	  asm volatile ("wfi" : : : "memory");
-	  /* NOTE: it never comes here.  Don't add lines after this.  */
-	}
+      chx_prepare_sleep_mode ();
+
+      asm volatile ("wfi" : : : "memory");
+      /* NOTE: it never comes here.  Don't add lines after this.  */
     }
 }
 
