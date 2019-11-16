02001f31c7657db1ba7fd12ec72ec172c40d7241
NIIBE Yutaka <gniibe@fsij.org>
2019-11-16 10:54:45 +0900

    riscv: Move chx_idle into chopstx-riscv32.c.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- chopstx-riscv32.c
+++ chopstx-riscv32.c
@@ -397,6 +397,25 @@ chx_init_arch (struct chx_thread *tp)
 	"lw	a1,44(sp)\n\t"
 
 
+
+static void __attribute__((naked))
+chx_idle (void)
+{
+  /*TBD*/
+  int sleep_enabled;
+
+  for (;;)
+    {
+      asm ("lw	%0,%1" : "=r" (sleep_enabled): "m" (chx_allow_sleep));
+      if (sleep_enabled)
+	{
+	  asm volatile ("wfi" : : : "memory");
+	  /* NOTE: it never comes here.  Don't add lines after this.  */
+	}
+    }
+}
+
+
 static uintptr_t
 voluntary_context_switch (struct chx_thread *tp_next)
 {
--- mcu/chx-gd32vf103.c
+++ mcu/chx-gd32vf103.c
@@ -9,20 +9,3 @@ chx_sleep_mode (int how)
   /*TBD*/
   (void)how;
 }
-
-void __attribute__((naked))
-chx_idle (void)
-{
-  /*TBD*/
-  int sleep_enabled;
-
-  for (;;)
-    {
-      asm ("lw	%0,%1" : "=r" (sleep_enabled): "m" (chx_allow_sleep));
-      if (sleep_enabled)
-	{
-	  asm volatile ("wfi" : : : "memory");
-	  /* NOTE: it never comes here.  Don't add lines after this.  */
-	}
-    }
-}
