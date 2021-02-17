68b78a0ade80df42490964600c5bc36b477f5e43
NIIBE Yutaka <gniibe@fsij.org>
2021-02-17 10:12:08 +0900

    GNU/Linux: Fix AckBtn driver, again.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- contrib/ackbtn-gnu-linux.c
+++ contrib/ackbtn-gnu-linux.c
@@ -79,6 +79,8 @@ user_interaction (void *arg)
   pollfds[1].fd = event_fd;
   pollfds[1].events = POLLIN;
 
+  fputs ("User interaction thread for AckBtn started.\n", stdout);
+
   while (1)
     {
       unsigned int acked_or_timeout = 0;
@@ -88,8 +90,6 @@ user_interaction (void *arg)
       while (!enabled)
 	pthread_cond_wait (&cond, &mutex);
       pthread_mutex_unlock (&mutex);
-
-      fputs ("User interaction for AckBtn started.\n", stdout);
 
       /* Consume all input if any.  */
       while (1)
@@ -116,6 +116,7 @@ user_interaction (void *arg)
 	}
 
       fputs ("Type RET to acknowledge (or wait for timeout) > ", stdout);
+      fflush (stdout);
 
       while (!acked_or_timeout)
 	{
